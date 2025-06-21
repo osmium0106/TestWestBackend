1cb1301ce046e6c2cfb673d31aa859f08e765da4
Osmium01 <aman.12011855@lpu.in>
2025-06-21 07:22:17 +0530

    cors allowed o

--- core/settings.py
+++ core/settings.py
@@ -152,7 +152,12 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    "https://testwest.live",
+    "https://www.testwest.live",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
