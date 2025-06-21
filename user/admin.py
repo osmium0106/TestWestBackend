4f45cbb3f341f955a602476df15cb8dde9c03f74
Osmium01 <aman.12011855@lpu.in>
2025-06-21 07:27:18 +0530

    admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserPreference
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role & Limits', {
+            'fields': ('role', 'max_sub_users', 'max_question_papers', 'parent')
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role & Limits', {
+            'fields': ('role', 'max_sub_users', 'max_question_papers', 'parent')
+        }),
+    )
+    list_display = ('username', 'email', 'role', 'max_sub_users', 'parent', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+
+admin.site.register(UserPreference)
